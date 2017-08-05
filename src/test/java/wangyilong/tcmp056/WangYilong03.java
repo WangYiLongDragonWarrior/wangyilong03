8dd635fbfa89c1cca6c01ec57f2538adf57713ef
WangYiLongWarrior <969789835@qq.com>
2017-08-05 20:56:54 +0800

    Initial commit

---
+++ src/test/java/wangyilong/tcmp056/WangYilong03.java
@@ -0,0 +1,14 @@
+package wangyilong.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/5.
+ */
+public class WangYilong03 {
+
+    @Test
+    public void init(){
+        System.out.println("Hello World");
+    }
+}
